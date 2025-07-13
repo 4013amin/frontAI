6f0a8e1ca3b9b7df5f7193754a964774e7af3a06
Alireza Hosseini <mehdimj0161@gmail.com>
2025-07-13 12:50:18 +0330

    feat: add shadcn tabs to project

--- src/components/sections/plan/Plans.tsx
+++ src/components/sections/plan/Plans.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import PageHeader from "@/components/ui/PageHeader"
 
+
 const Plans = () => {
   return (
     <div id="plans">
-      <PageHeader title="اشتراک من" >
+      <PageHeader title="اشتراک من" />
+
         
-      </PageHeader>
     </div>
   )
 }
