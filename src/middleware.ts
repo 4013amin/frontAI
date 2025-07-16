c85e0709008fa97691bbe7cf5e6df1f5b534af8a
Alirea Hosseini <mehdimj0161@gmail.com>
2025-07-16 17:08:27 +0330

    refactor(middleware): rename auth token

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,17 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export function middleware(request: NextRequest) {
-  const localToken = request.cookies.get("local_token")?.value
+  const authToken = request.cookies.get("auth_token")?.value
   const pathname = request.nextUrl.pathname
 
   //   if not login redirect to login page
-  if (pathname.startsWith("/panel") && !localToken) {
+  if (pathname.startsWith("/panel") && !authToken) {
     return NextResponse.redirect(new URL("/auth/login", request.url))
   }
 
   //   if login redirect to panel page
   if (
-    (pathname.startsWith("/auth")) && localToken === "true"
+    (pathname.startsWith("/auth")) && authToken
   ) {
     return NextResponse.redirect(new URL("/panel", request.url))
   }
