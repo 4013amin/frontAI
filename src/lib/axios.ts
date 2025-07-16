e1a5a7647c6ce7926593b706f9279ade9232b14b
Alirea Hosseini <mehdimj0161@gmail.com>
2025-07-16 17:09:39 +0330

    refactor(axios config): create interceptors for handel login and logout aotomatic

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,7 @@
 // api.ts
 import axios from "axios"
-import { logoutUser } from "./logoutUser"
+import Cookies from "js-cookie"
+import { toast } from "sonner"
 
 const API = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
@@ -12,13 +13,43 @@ const API = axios.create({
   }
 })
 
-// ⛔️ Global response interceptor to handle 401 errors
+// 🟢 Request interceptor: Atomatic add token in headers
+API.interceptors.request.use(
+  config => {
+    const token = Cookies.get("auth_token")
+    if (token) {
+      config.headers.Authorization = `Token ${token}`
+    }
+    return config
+  },
+  error => Promise.reject(error)
+)
+
+// 🔴 Response interceptor: 401 error | logout user
 API.interceptors.response.use(
   response => response,
   error => {
     if (typeof window !== "undefined" && error.response?.status === 401) {
-      logoutUser()
+      // remove token
+      Cookies.remove("auth_token")
+
+      toast.error("نشست شما منقضی شده! دوباره وارد شوید")
+
+      // clear everythings
+      localStorage.clear()
+      sessionStorage.clear()
+      if ("caches" in window) {
+        caches.keys().then(names => {
+          names.forEach(name => caches.delete(name))
+        })
+      }
+
+      // redirect to login page
+      setTimeout(() => {
+        window.location.href = "/auth/login"
+      }, 3000)
     }
+
     return Promise.reject(error)
   }
 )
