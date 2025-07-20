a5b2754759a031a5cd632468a16d1bcddc6f676c
Alireza Hosseini <mehdimj0161@gmail.com>
2025-07-20 10:39:48 +0330

    feat(payments): create empty list dialog

--- src/components/sections/payments-list/PaymentsList.tsx
+++ src/components/sections/payments-list/PaymentsList.tsx
@@ -1,5 +1,7 @@
 "use client"
 import React from "react"
+import { CircleCheck } from "lucide-react"
+import Link from "next/link"
 import PaymentItem from "./PaymentItem"
 import PaymentsListSkeleton from "./PaymentsListSkeleton"
 import useGetPayments from "@/hooks/useGetPayments"
@@ -27,17 +29,41 @@ const PaymentsList = () => {
               <NotLoadErorr />
             )
             : (
-              <div className="grid !grid-cols-1 lg:!grid-cols-3 gap-5 w-full items-start mb-10">
-                {
-                  payments?.map((payment: IPayment) => (
-                    <PaymentItem  
-                      key={payment.id}
-                      payment={payment}
-                    />
+              payments?.length
+                ? (
+                  <div className="grid !grid-cols-1 lg:!grid-cols-3 gap-5 w-full items-start mb-10">
+                    {
+                      payments?.map((payment: IPayment) => (
+                        <PaymentItem  
+                          key={payment.id}
+                          payment={payment}
+                        />
 
-                  ))
-                }
-              </div>
+                      ))
+                    }
+                  </div>
+                )
+                : (
+                  <div className="my-12 flex-center flex-col gap-2">
+                    <CircleCheck size={32} />
+
+                    <h2 className="font-bold text-lg mt-3">لیست پرداخت های شما خالی است</h2>
+
+                    <span
+                      className="text-sm text-zinc-600 
+                      dark:text-zinc-400"
+                    >
+                      برای مشاهده پلن ها روی دکمه زیر کلیک کنید
+                    </span>
+
+                    <Link 
+                      href="/panel/subscription"
+                      className="bg-blue-500 text-white py-2 px-3 rounded-lg text-sm mt-3"
+                    >
+                      مشاهده اشتراک ها
+                    </Link>
+                  </div>
+                )
             )
       }
 
