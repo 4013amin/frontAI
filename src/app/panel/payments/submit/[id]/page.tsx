2294ed006e085fc851d6503067ace2d0643987ee
Alireza Hosseini <mehdimj0161@gmail.com>
2025-07-21 15:51:46 +0330

    fixed deploy bug

--- src/app/panel/payments/submit/[id]/page.tsx
+++ src/app/panel/payments/submit/[id]/page.tsx
@@ -1,18 +1,15 @@
+"use client"
+
+import { useParams } from "next/navigation"
 import PaymentDetails from "@/components/sections/submit-payment/PaymentDetails"
 import PaymentSteps from "@/components/sections/submit-payment/PaymentSteps"
 import SubmitPaymentForm from "@/components/sections/submit-payment/SubmitPaymentForm"
 import Breadcrumb from "@/components/ui/Breadcrumb"
 import PageHeader from "@/components/ui/PageHeader"
 
-
-interface PageProps {
-  params: {
-    id: string
-  }
-}
-
-export default async function Page({ params }: PageProps) {
-  const { id } = params
+const Page = () => {
+  const params = useParams()
+  const id = params?.id
 
   const breadcrumbItems = [
     { title: "پرداخت ها", link: "/panel/payments" },
@@ -20,7 +17,6 @@ export default async function Page({ params }: PageProps) {
   ]
 
   return (
-
     <div className="mb-10">
       <Breadcrumb items={breadcrumbItems} />
 
@@ -30,11 +26,9 @@ export default async function Page({ params }: PageProps) {
         id && (
           <div
             className="flex flex-col lg:!flex-row gap-5 w-full 
-            justify-start items-start"
+          justify-start items-start"
           >
-            <div
-              className="w-full lg:!w-3/5 flex flex-col items-start justify-start"
-            >
+            <div className="w-full lg:!w-3/5 flex flex-col items-start justify-start">
               <PaymentSteps />
 
               <SubmitPaymentForm planId={Number(id)} />
@@ -42,14 +36,15 @@ export default async function Page({ params }: PageProps) {
 
             <div
               className="w-full lg:!w-2/5 flex flex-col items-start justify-start p-3
-              border rounded-xl"
+            border rounded-xl"
             >
               <PaymentDetails planId={Number(id)} />
             </div>
           </div>
         )
       }
-      
     </div>
   )
 }
+
+export default Page
